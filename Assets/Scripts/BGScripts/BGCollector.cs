80fab8ff38290988f597c4a5ee6c868a316fffc4
Ahere George Odongo <georgeahere@gmail.com>
2016-03-01 02:57:07 +0800

    BackGroundSpawner
    Added the background spawner. The Cloud spawner is next

---
+++ Assets/Scripts/BGScripts/BGCollector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGCollector : MonoBehaviour {
+
+	// Use this for initialization
+	
+
+	void OnTriggerEnter2D(Collider2D target) {
+
+		if (target.tag =="Background") {
+			Debug.Log("shit went down");
+			 //if the target is background , deactivate it
+			target.gameObject.SetActive(false);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/CameraScripts/CameraScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public float speed = 1.0f;
+	public float acceleration = 0.2f;
+	public float maxSpeed = 3.2f;
+	public float smooth = 0.1f;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	MoveCameraByLerp ();
+
+		
+
+	}
+
+
+
+	void MoveCameraByLerp() {
+
+		Vector3 temp = transform.position;
+
+		temp.y = Mathf.Lerp (temp.y, temp.y - (speed * Time.deltaTime), smooth);
+
+		transform.position = temp;
+		
+		speed += acceleration * Time.deltaTime;
+		
+		if (speed > maxSpeed)
+			speed = maxSpeed;
+
+
+	}
+
+	void MoveCamera() {
+
+		Vector3 temp = transform.position;
+
+		float oldY = temp.y;
+
+		float newY = temp.y - (speed * Time.deltaTime);
+
+		temp.y = Mathf.Clamp (temp.y, oldY, newY);
+
+		transform.position = temp;
+
+		speed += acceleration * Time.deltaTime;
+
+		if (speed > maxSpeed)
+				speed = maxSpeed;
+
+	}
+
+	
+}
+
